8fab5ff5ae51b63beb2d1a587a8c588adbdb7ce9
Dan Hammond <dan@hammond.email>
2023-08-09 19:07:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Repro for https://github.com/nuxt/nuxt/issues/22564
+
+See ~/src/runtime/TemplatePage.vue
---
+++ src/runtime/TemplatePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>hello {{ world }}</div>
+</template>
+
+<script lang="ts" setup>
+import {
+  computed,
+} from "#imports";
+
+const world = computed( () => "world" );
+</script>
